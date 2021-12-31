f1630cc8f61e2c5fd01d0425cb7eeb3911e912ea
Ngange <ngangebabu@gmail.com>
2021-12-31 01:01:42 +0000

    3.17*: Phonebook database, step5

--- index.js
+++ index.js
@@ -98,6 +98,21 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true})
+    .then(updated => {
+      response.json(updated)
+    })
+    .catch(error => next(error))
+})
+
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
