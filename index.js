1ffcf97aff6a6edb0d1622e06e1e31844b10f374
Ngange <ngangebabu@gmail.com>
2021-12-31 02:52:33 +0000

    3.19: Phonebook database, step7

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'mal-formatted id'})
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message})
   }
 
   next(error)
@@ -71,7 +73,7 @@ app.get('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', morgan(':body'), (request, response) => {
+app.post('/api/persons', morgan(':body'), (request, response, next) => {
   const body = request.body
   
   if (!body.name || !body.number) {
@@ -87,7 +89,7 @@ app.post('/api/persons', morgan(':body'), (request, response) => {
 
   person.save().then(saved => {
     response.json(saved)
-  })
+  }).catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
