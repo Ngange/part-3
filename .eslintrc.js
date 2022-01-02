778c640c950592e1f5c18a61aa5f667979f6bf30
Ngange <ngangebabu@gmail.com>
2022-01-02 00:51:54 +0000

    3.22: Lint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+module.exports = {
+	'env': {
+		'commonjs': true,
+		'es2021': true,
+		'node': true
+	},
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 13
+	},
+	'rules': {
+		'eqeqeq': 'error',
+		'no-trailing-spaces': 'error',
+    	'object-curly-spacing': [
+        	'error', 'always'
+    	],
+    	'arrow-spacing': [
+        	'error', { 'before': true, 'after': true }
+    	],
+    	'no-console': 0,
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		]
+	}
+}
--- index.js
+++ index.js
@@ -10,116 +10,116 @@ app.use(express.json())
 app.use(cors())
 
 //token for request body
-morgan.token('body', (request, response) => JSON.stringify(request.body));
+morgan.token('body', (request) => JSON.stringify(request.body))
 app.use(morgan('tiny'))
 
 const errorHandler = (error, request, response, next) => {
-  console.log(error.message)
+	console.log(error.message)
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'mal-formatted id'})
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message})
-  }
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'mal-formatted id' })
+	} else if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message })
+	}
 
-  next(error)
+	next(error)
 }
 
 let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
+	{
+		'id': 1,
+		'name': 'Arto Hellas',
+		'number': '040-123456'
+	},
+	{
+		'id': 2,
+		'name': 'Ada Lovelace',
+		'number': '39-44-5323523'
+	},
+	{
+		'id': 3,
+		'name': 'Dan Abramov',
+		'number': '12-43-234345'
+	},
+	{
+		'id': 4,
+		'name': 'Mary Poppendieck',
+		'number': '39-23-6423122'
+	}
 ]
 
 app.get('/api/persons', (request, response)  => {
-  Person.find({}).then(person => {
-    response.json(person)
-  })
+	Person.find({}).then(person => {
+		response.json(person)
+	})
 })
 
 app.get('/info', (request, response) => {
-  let contacts = persons.length
-  const date = new Date()
+	let contacts = persons.length
+	const date = new Date()
 
-  response.send(`Phonebook has info for ${contacts} people <br/><br/> ${date}`)
+	response.send(`Phonebook has info for ${contacts} people <br/><br/> ${date}`)
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-  Person.findById(request.params.id)
-    .then(person => {
-      if (person) {
-        response.json(person)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+	Person.findById(request.params.id)
+		.then(person => {
+			if (person) {
+				response.json(person)
+			} else {
+				response.status(404).end()
+			}
+		})
+		.catch(error => next(error))
 })
 
 app.post('/api/persons', morgan(':body'), (request, response, next) => {
-  const body = request.body
-  
-  // if (!body.name || !body.number) {
-  //   return response.status(400).json({
-  //     error: 'name or number cannot be empty!'
-  //   })
-  // }
+	const body = request.body
 
-  const person = new Person({
-    name: body.name,
-    number: body.number,
-  })
+	// if (!body.name || !body.number) {
+	//   return response.status(400).json({
+	//     error: 'name or number cannot be empty!'
+	//   })
+	// }
 
-  person.save().then(saved => {
-    response.json(saved)
-  }).catch(error => next(error))
+	const person = new Person({
+		name: body.name,
+		number: body.number,
+	})
+
+	person.save().then(saved => {
+		response.json(saved)
+	}).catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-  Person.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+	Person.findByIdAndRemove(request.params.id)
+		.then(() => {
+			response.status(204).end()
+		})
+		.catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+	const body = request.body
 
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
+	const person = {
+		name: body.name,
+		number: body.number,
+	}
 
-  Person.findByIdAndUpdate(request.params.id, person, { runValidators: true, context: 'query', new: true })
-    .then(updated => {
-      response.json(updated)
-    })
-    .catch(error => next(error))
+	Person.findByIdAndUpdate(request.params.id, person, { runValidators: true, context: 'query', new: true })
+		.then(updated => {
+			response.json(updated)
+		})
+		.catch(error => next(error))
 })
 
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
+	console.log(`server running on port ${PORT}`)
 })
 
 
